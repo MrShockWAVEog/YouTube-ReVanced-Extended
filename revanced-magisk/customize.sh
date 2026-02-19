8f666ca416151995eda974cd8c1e49ca93fd6332
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2026-02-19 15:40:37 +0600

    Update customize.sh

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -195,5 +195,5 @@ fi
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
 ui_print "* Done"
-ui_print "  by CrimSonMAN (github.com/MANCrimSon)"
+ui_print "  by MrShockWAVEog (github.com/MrShockWAVEog)"
 ui_print " "
